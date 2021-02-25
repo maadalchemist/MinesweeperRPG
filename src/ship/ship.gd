fbde4bf691a250f599c6e0485c77d4b138586a40
maad_alchemist <josephwardle02@gmail.com>
2021-02-25 12:03:50 -0700

    stuff

--- src/ship/ship.gd
+++ src/ship/ship.gd
@@ -7,8 +7,9 @@ var rot_velocity = 0;
 export var rot_acc = .002
 export var rot_fric = .003
 export var max_rot_velocity = .1
-export var acc = 10
-export var speed = 100
+export var acc = 1
+export var fric = .001
+export var max_speed = Vector2(50, 0)
 
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
@@ -22,5 +23,8 @@ func _physics_process(delta):
 	rotation += rot_velocity
 	
 	# velocity = input_vector * speed
-	
+	# if input_vector.y != 0:
+	velocity.move_toward(max_speed, acc)
+	# velocity.move_toward(Vector2.ZERO, fric)
+	velocity = Vector2(cos(rotation) * 50, sin(rotation) * 50)
 	move_and_slide(velocity)
