261f60894290d021116a73177c4df7c45346fdce
maad_alchemist <josephwardle02@gmail.com>
2021-02-27 22:45:22 -0700

    partial work on the particle effects

---
+++ src/ship/engine_splash.gd
@@ -0,0 +1,5 @@
+extends Particles2D
+
+
+func _update_splash_count(splash_amount):
+	amount = splash_amount
--- src/ship/ship.gd
+++ src/ship/ship.gd
@@ -2,14 +2,21 @@ extends KinematicBody2D
 
 
 var velocity = Vector2.ZERO
-var rot_velocity = 0;
+var rot_velocity = 0
+var splash_amount = 0
+var old_splash_amount = 0
 
 export var rot_acc = .002
 export var rot_fric = .002
 export var max_rot_velocity = .05
 export var acc = .02
 export var fric = .7
-export var max_speed = 70;
+export var max_speed = 70
+export var max_splash_amount = 200
+
+signal splash_count
+
+onready var engine_sound = $engine_sound
 
 
 func _physics_process(delta):
@@ -19,7 +26,7 @@ func _physics_process(delta):
 	
 	# rotational input
 	if input_vector.x != 0:
-		rot_velocity = move_toward(rot_velocity, max_rot_velocity * sign(input_vector.x), rot_acc)
+		rot_velocity = move_toward(rot_velocity, max_rot_velocity * sign(input_vector.x), .0003 + (acc * (velocity.length() / (max_speed * 5))))
 	else:
 		rot_velocity = move_toward(rot_velocity, 0, rot_fric)
 	rotation += rot_velocity
@@ -31,3 +38,12 @@ func _physics_process(delta):
 	velocity = velocity.clamped(max_speed)
 	move_and_slide(velocity)
 	
+	if input_vector.y != 0:
+		engine_sound.pitch_scale = move_toward(engine_sound.pitch_scale, 5, .05)
+		splash_amount = move_toward(splash_amount, max_splash_amount, 2)
+	else:
+		engine_sound.pitch_scale = move_toward(engine_sound.pitch_scale, 1, .1)
+		splash_amount = move_toward(splash_amount, 0, 4)
+	if old_splash_amount != splash_amount:
+		emit_signal("splash_count", splash_amount)
+	old_splash_amount = splash_amount
