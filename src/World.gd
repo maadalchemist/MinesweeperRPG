a10c0cb4f9ec18defff99edb2569956265fb5713
maad_alchemist <josephwardle02@gmail.com>
2021-03-15 12:26:19 -0600

    edits

--- src/World.gd
+++ src/World.gd
@@ -31,6 +31,14 @@ func _ready():
 	for i in range(-5, 5):
 		for j in range(-5, 5):
 			minefield[Vector2(i, j)] = MINE
+	
+	var camera_position = Camera2D.get_camera_position()
+	var camera_grid_position = Vector2(int(camera_position.x / 16), int(camera_position.y / 16))
+	for i in range(-12, 12):
+		for j in range(-8, 8):
+			generate_mine_at(Vector2(i + camera_grid_position.x, j + camera_grid_position.y))
+	
+	reveal_cell(Vector2(0,0))
 
 func _physics_process(delta):
 	var camera_position = Camera2D.get_camera_position()
@@ -40,22 +48,30 @@ func _physics_process(delta):
 			generate_mine_at(Vector2(i + camera_grid_position.x, j + camera_grid_position.y))
 
 func _process(delta):
+	# Water shader
 	water.position = Camera2D.position;
 	water.material.set_shader_param("global_position", water.get_global_position())
 	water.material.set_shader_param("scale", water.scale)
+	# Grid shader
 	grid.position = Camera2D.position;
 	grid.material.set_shader_param("global_position", grid.get_global_position())
 	grid.material.set_shader_param("scale", water.scale)
 
+# Generates mine at vector grid position
 func generate_mine_at(pos):
-	if pos in minefield:
-		return
+	if pos in minefield: # if already generated
+		return 
 	var mine_seed = randi() % 100
 	if mine_seed <= mine_chance:
 		new_mine(pos)
 	else:
 		minefield[pos] = SAFE
+		if not pos in verbose_minefield:
+			for i in range(-1, 1):
+				for j in range(-1, 1):
+					generate_mine_at(Vector2(pos.x + i, pos.y + j))
 
+# Places a mine and updates surrounding tiles
 func new_mine(pos):
 	minefield[pos] = MINE
 	for i in range(pos.x - 1, pos.x + 2):
@@ -66,20 +82,18 @@ func new_mine(pos):
 					verbose_minefield[check] += 1
 			else:
 				verbose_minefield[check] = 1
-			update_number_tile(check)
+			#update_number_tile(check)
 	verbose_minefield[pos] = MINE
-	update_number_tile(pos)
-
-#func perimeter_mine_count(pos):
-#	var count = 0
-#	if pos in minefield:
-#		if minefield[pos] == MINE:
-#			return count
-#	for i in range(pos.x - 1, pos.x + 2):
-#		for j in range(pos.y - 1, pos.y + 2):
-#			if Vector2(i, j) in minefield:
-#				count += (minefield[Vector2(i, j)] == MINE)
-#	return count
+	#update_number_tile(pos)
 
 func update_number_tile(pos):
+	NumberTileset.set_cell(pos.x, pos.y, 0, false, false, false, Vector2(verbose_minefield[pos],0))
+
+func reveal_cell(pos):
+	if not pos in verbose_minefield:
+		for i in range(-1, 1):
+			for j in range(-1, 1):
+				reveal_cell(Vector2(pos.x + i, pos.y + j))
+		print("that tile hasn't been generated yet!")
+		return
 	NumberTileset.set_cell(pos.x, pos.y, 0, false, false, false, Vector2(verbose_minefield[pos],0))
--- src/ship/ship.gd
+++ src/ship/ship.gd
@@ -14,13 +14,14 @@ export var fric = .7
 export var max_speed = 70
 export var max_splash_amount = .5
 
-signal splash_count
-signal turn
+signal splash_count # for particle count
+signal turn # for particle angles
 
 onready var engine_sound = $engine_sound
 
 
 func _physics_process(delta):
+	# determine inputs
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
@@ -28,7 +29,7 @@ func _physics_process(delta):
 	# rotational input
 	if input_vector.x != 0:
 		rot_velocity = move_toward(rot_velocity, max_rot_velocity * sign(input_vector.x), .0003 + (acc * (velocity.length() / (max_speed * 5))))
-		emit_signal("turn_direction",input_vector.x)
+		emit_signal("turn_direction",input_vector.x) 
 	else:
 		rot_velocity = move_toward(rot_velocity, 0, rot_fric)
 		emit_signal("turn_direction",0)
@@ -39,8 +40,13 @@ func _physics_process(delta):
 		velocity += -sign(input_vector.y) * max_speed * Vector2(cos(rotation), sin(rotation)) * acc
 	velocity = velocity.move_toward(Vector2.ZERO, fric)
 	velocity = velocity.clamped(max_speed)
-	move_and_slide(velocity)
 	
+	# movement
+	var collision = move_and_collide(velocity * delta)
+	if collision:
+		velocity = velocity.slide(collision.normal)
+	
+	# effects
 	if input_vector.y != 0:
 		engine_sound.pitch_scale = move_toward(engine_sound.pitch_scale, 3, .05)
 		splash_amount = move_toward(splash_amount, max_splash_amount, .005)
