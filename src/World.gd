26607290b02a07a8ea5b733d6e2d9179e8addd59
maad_alchemist <josephwardle02@gmail.com>
2021-03-17 00:08:03 -0700

    simplex noise mine generation WIP

--- src/World.gd
+++ src/World.gd
@@ -1,10 +1,11 @@
 extends Node2D
 
 
-export var mine_chance = 10
+export var mine_threshold = -.25
+export var debug_print_minefield = false
 
 enum {
-	SAFE,
+	ZERO,
 	ONE,
 	TWO,
 	THREE,
@@ -16,8 +17,16 @@ enum {
 	MINE,
 }
 
+enum {
+	RED,
+	YELLOW,
+	GREEN,
+}
+
 var minefield = {}
-var verbose_minefield = {}
+var visible_field = {}
+var minefield_chunks = {}
+var noise = OpenSimplexNoise.new()
 
 onready var NumberTileset = $NumberTileset
 onready var grid = $Grid
@@ -26,28 +35,23 @@ onready var water = $Water
 
 
 func _ready():
-	randomize()
-	
-	for i in range(-5, 5):
-		for j in range(-5, 5):
-			minefield[Vector2(i, j)] = MINE
+#	randomize()
+	noise.octaves = 100
+	noise.period = 1.0
+	noise.persistence = 0.8
 	
 	var camera_position = Camera2D.get_camera_position()
-	var camera_grid_position = Vector2(int(camera_position.x / 16), int(camera_position.y / 16))
-	for i in range(-12, 12):
-		for j in range(-8, 8):
-			generate_mine_at(Vector2(i + camera_grid_position.x, j + camera_grid_position.y))
+	var camera_grid_pos = Vector2(int(camera_position.x / 16), int(camera_position.y / 16))
+	var camera_chunk_pos = camera_grid_pos / 16
 	
-	reveal_cell(Vector2(0,0))
-
-func _physics_process(delta):
-	var camera_position = Camera2D.get_camera_position()
-	var camera_grid_position = Vector2(int(camera_position.x / 16), int(camera_position.y / 16))
-	for i in range(-12, 12):
-		for j in range(-8, 8):
-			generate_mine_at(Vector2(i + camera_grid_position.x, j + camera_grid_position.y))
+	for i in range(-2,1):
+		for j in range(-2,1):
+			generate_chunk(Vector2(i,j))
 
 func _process(delta):
+	var camera_position = Camera2D.get_camera_position()
+	var camera_grid_pos = Vector2(int(camera_position.x / 16), int(camera_position.y / 16))
+	var camera_chunk_pos = camera_grid_pos / 16
 	# Water shader
 	water.position = Camera2D.position;
 	water.material.set_shader_param("global_position", water.get_global_position())
@@ -57,43 +61,58 @@ func _process(delta):
 	grid.material.set_shader_param("global_position", grid.get_global_position())
 	grid.material.set_shader_param("scale", water.scale)
 
-# Generates mine at vector grid position
-func generate_mine_at(pos):
-	if pos in minefield: # if already generated
-		return 
-	var mine_seed = randi() % 100
-	if mine_seed <= mine_chance:
-		new_mine(pos)
-	else:
-		minefield[pos] = SAFE
-		if not pos in verbose_minefield:
-			for i in range(-1, 1):
-				for j in range(-1, 1):
-					generate_mine_at(Vector2(pos.x + i, pos.y + j))
-
-# Places a mine and updates surrounding tiles
-func new_mine(pos):
-	minefield[pos] = MINE
-	for i in range(pos.x - 1, pos.x + 2):
-		for j in range(pos.y - 1, pos.y + 2):
-			var check = Vector2(i, j)
-			if check in verbose_minefield:
-				if verbose_minefield[check] != MINE:
-					verbose_minefield[check] += 1
-			else:
-				verbose_minefield[check] = 1
-			#update_number_tile(check)
-	verbose_minefield[pos] = MINE
-	#update_number_tile(pos)
-
-func update_number_tile(pos):
-	NumberTileset.set_cell(pos.x, pos.y, 0, false, false, false, Vector2(verbose_minefield[pos],0))
-
-func reveal_cell(pos):
-	if not pos in verbose_minefield:
-		for i in range(-1, 1):
-			for j in range(-1, 1):
-				reveal_cell(Vector2(pos.x + i, pos.y + j))
-		print("that tile hasn't been generated yet!")
+# Generates a chunk all the way to green state
+func generate_chunk(chunk):
+	if not chunk in minefield_chunks:
+		generate_chunk_red(chunk)
+	elif minefield_chunks[chunk] == GREEN:
 		return
-	NumberTileset.set_cell(pos.x, pos.y, 0, false, false, false, Vector2(verbose_minefield[pos],0))
+	elif minefield_chunks[chunk] == RED:
+		generate_chunk_red(chunk)
+	if minefield_chunks[chunk] == YELLOW:
+		generate_chunk_yellow(chunk)
+
+# first chunk generation step - does not check for proper edge tile states
+func generate_chunk_red(chunk):
+	if not chunk in minefield_chunks:
+		minefield_chunks[chunk] = RED
+	for i in range(0,16):
+		for j in range(0,16):
+			var pos = chunk * 16
+			pos.x += i
+			pos.y += j
+			var noise_val = noise.get_noise_2dv(pos)
+			
+			if not pos in minefield:
+				minefield[pos] = ZERO
+			if noise_val <= mine_threshold:
+				minefield[pos] = MINE
+				for k in range (-1,2):
+					for l in range(-1,2):
+						var neighbor = Vector2(k,l) + pos
+						if not neighbor in minefield:
+							minefield[neighbor] = 0
+						if minefield[neighbor] == MINE:
+							continue
+						else:
+							minefield[neighbor] += 1
+	minefield_chunks[chunk] = YELLOW
+
+#second chunk generation step - generates edge chunks
+func generate_chunk_yellow(chunk):
+	if not chunk in minefield_chunks or minefield_chunks[chunk] == RED:
+		generate_chunk_red(chunk)
+	for i in range (-1,1):
+		for j in range(-1,1):
+			if not chunk in minefield_chunks or minefield_chunks[chunk] == RED:
+				generate_chunk_red(chunk)
+	# Debug
+	if debug_print_minefield:
+		for i in range(0,16):
+			for j in range(0,16):
+				var pos = chunk * 16
+				pos.x += i
+				pos.y += j
+				NumberTileset.set_cell(pos.x, pos.y, 0, false, false, false, Vector2(minefield[pos],0))
+	
+	minefield_chunks[chunk] = GREEN
